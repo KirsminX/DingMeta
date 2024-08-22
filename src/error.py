16941ed489086cc8a31533e58c57d14fbee1305e
KirsminX <dino_doge@qq.com>
2024-08-22 18:40:22 +0800

    <add> 错误类型

--- src/error.py
+++ src/error.py
@@ -2,3 +2,8 @@ class DownloadError(Exception):
     def __init__(self, message: str):
         self.message = message
         super().__init__(self.message)
+
+class InputError(Exception):
+    def __init__(self, message: str):
+        self.message = message
+        super().__init__(self.message)
