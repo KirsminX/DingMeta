a9cdc93d56733f15ba54499313c4dc37a55d749b
KirsminX <dino_doge@qq.com>
2024-09-18 19:28:50 +0800

    <optimize> 完善日志模块

--- src/log.py
+++ src/log.py
@@ -9,7 +9,7 @@ import pytz
 from datetime import datetime
 
 class Log:
-    def __init__(self, written: bool = True, memorize: bool = True, timezone: str = "Asia/Shanghai"):
+    def __init__(self, debug: bool,written: bool = True, memorize: bool = True, timezone: str = "Asia/Shanghai"):
         if written and not os.path.isfile("Log"):
             open("Log", "w").close()
         self.written = written
@@ -30,13 +30,15 @@ class Log:
             "亥时": (21, 23)
         }
         self.color = {
+            "DEBUG": '<p style="color: rgb(135, 133, 162);"[调试]</p>',
             "INFO": '<p style="color: rgb(0, 184, 169);">[信息]</p>',
             "WARNING": '<p style="color: rgb(255, 222, 125);">[警告]</p>',
             "ERROR": '<p style="color: rgb(246, 65, 108);">[错误]</p>'
         }
-
-    def __get_formatted_time__(self, timezone: str = "Asia/Shanghai") -> str:
-        current_time = datetime.now(pytz.timezone(timezone))
+        self.debug_statu = debug
+        self.timezone = timezone
+    def __get_formatted_time__(self) -> str:
+        current_time = datetime.now(pytz.timezone(self.timezone))
         hour = current_time.hour
         minute = current_time.minute
         second = current_time.second
@@ -53,8 +55,36 @@ class Log:
             file.write(f"{msg}\n")
     @staticmethod
     def info(self, msg: str):
-        print(HTML(f"{self.color['INFO']}{msg}"))
+        print(f"{self.__get_display_msg__('INFO', msg)}")
         if self.written:
-            self.__write__(f"[INFO] {msg}")
+            self.__write__(f"[信息] {msg}")
         if self.memorize:
-            self.logs.append(f"[INFO] {msg}")
+            self.logs.append(f"[信息] {msg}")
+
+    def debug(self, msg: str):
+        if self.debug_statu:
+            print(f"{self.__get_display_msg__('DEBUG', msg)}")
+            if self.written:
+                self.__write__(f"[调试] {msg}")
+            if self.memorize:
+                self.logs.append(f"[调试] {msg}")
+
+    def warning(self, msg: str):
+        print(f"{self.__get_display_msg__('WARNING', msg)}")
+        if self.written:
+            self.__write__(f"[警告] {msg}")
+        if self.memorize:
+            self.logs.append(f"[警告] {msg}")
+
+    def error(self, msg: str):
+        print(f"{self.__get_display_msg__('ERROR', msg)}")
+        if self.written:
+            self.__write__(f"[错误] {msg}")
+        if self.memorize:
+            self.logs.append(f"[错误] {msg}")
+
+    def __get_msg__(self, level: str, message: str):
+        return f"{self.__get_formatted_time__()} {level} {message}"
+
+    def __get_display_msg__(self, level: str, message: str):
+        return HTML(f"{self.__get_formatted_time__()} {self.color[level]} {message}")
