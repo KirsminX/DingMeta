0bfcadd603e0a10c2c0ab66cac5bf31660509619
KirsminX <dino_doge@qq.com>
2024-10-01 10:41:48 +0800

    <add> 完成初始化

--- src/main.py
+++ src/main.py
@@ -1,1 +1,23 @@
+from datetime import timezone
+import asyncio
+from log import Log
+from parse_config import Config
+"""
+DingMeta 源码
+作者    Kirsmin/KirsminX
+协议    MIT License
+版本    Alpha
+"""
 
+def main():
+    config = Config()
+    console_config = config.getter("Console")
+    if console_config["Log"]["log_mode"] == "file":
+        written = True
+    else:
+        written = False
+    log = Log(debug=console_config["Log"]["debug"], written=written, memorize=True, timezone=console_config["time_zone"])
+    log.info("Hello")
+
+if __name__ == "__main__":
+    main()
