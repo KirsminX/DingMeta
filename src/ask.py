3f092e03f239208c0c82b189ff72614957695575
KirsminX <dino_doge@qq.com>
2024-10-11 21:37:26 +0800

    <fix> 现在质询模块不再使用 Logging，改用 Log 模块

--- src/ask.py
+++ src/ask.py
@@ -1,17 +1,18 @@
 from prompt_toolkit import prompt
-import logging, error
+from log import Log; log = Log()
+import error
 
 """质询问题"""
-def ask(question: str, type: str = "info", yes: str = None, no: str = None, try_max_length: int = 3):
-    if type == "warning":
-        logging.warning(f"{question}？")
-    elif type == "info":
-        logging.info(f"{question}？")
+def ask(question: str, _type_: str = "info", yes: str = None, no: str = None, try_max_length: int = 3):
+    if _type_ == "warning":
+        log.warning(f"{question}？")
+    elif _type_ == "info":
+        log.info(f"{question}？")
     if yes and no is not None:
         question += f"[ Y ] {yes}\n[ N ]{no}"
     else:
-        raise error.InputError(f"质询问题类型错误！期望值「warning/info」得到「{type}」")
-    for i in range(try_max_length + 1):
+        raise error.InputError(f"质询问题类型错误！期望值「warning/info」得到「{_type_}」")
+    for i in range(try_max_length - 1):
         answer = prompt("[Y/N]").upper()
         if answer == "Y":
             return True
@@ -19,5 +20,5 @@ def ask(question: str, type: str = "info", yes: str = None, no: str = None, try_
             return False
         else:
             if i < 4:
-                logging.warning(f"输入值无效！[{i + 1}/{try_max_length}]")
+                log.warning(f"输入值无效！[{i + 1}/{try_max_length}]")
     return None     # 用户输入无效，返回None
