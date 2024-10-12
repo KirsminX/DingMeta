868db5337e481acbc1c2e08522f2d003297cae06
KirsminX <dino_doge@qq.com>
2024-10-12 19:16:13 +0800

    <add> 现在质询模块支持字典输入

--- src/ask.py
+++ src/ask.py
@@ -3,27 +3,24 @@ from log import Log; log = Log()
 import error
 
 """质询问题"""
-def ask(question: str, _type_: str = "info", yes: str = None, no: str = None, default: str = None, try_max_length: int = 3) -> bool or None:
-    if _type_ == "warning":
-        log.warning(f"{question}？")
-    elif _type_ == "info":
-        log.info(f"{question}？")
-    if yes and no is not None:
-        question += f"[ Y ] {yes}\n[ N ]{no}"
+def ask(question: str, options: dict, default: str = None, level: str = "info",try_max_length: int = 3) -> str:
+    if level == "warning":
+        log.warning(f"- {question} -")
+    elif level == "error":
+        log.error(f"- {question} -")
+    elif level == "info":
+        log.info(f"- {question} -")
     else:
-        raise error.InputError(f"质询问题类型错误！期望值「warning/info」得到「{_type_}」")
-    for i in range(try_max_length - 1):
-        if default is not None:
-            answer = prompt(f"[Y/N] (默认 「{default}」").upper()
-        else:
-            answer = prompt(f"[Y/N]").upper()
-        if answer == "Y":
-            return True
-        elif answer == "N":
-            return False
-        elif default is not None:
+        raise error.InputError(f"不存在该等级：{level}")
+    for key, value in options.items():
+        log.info(f"[ {key} ] {value}")
+    option_key = "/".join(list(options.keys()))
+    for _ in range(try_max_length):
+        answer = prompt(f"[{option_key}] →")
+        if answer in options:
+            return answer
+        if answer == "" and default is not None:
             log.info(f"使用默认值 「{default}」")
             return default
-        else:
-            if i < 4:
-                log.warning(f"输入值无效！[{i + 1}/{try_max_length}]")
+        log.warning(f"输入值不正确！[{_+1}/{try_max_length}")
+    log.error(f"错误 {try_max_length} 次")
